2072abe04dae240fcf71c80fb8b340c88f697a7d
Monika Borawska <monikabor20@wp.pl>
2018-02-13 22:10:45 +0100

    Wstępna logika kalkulatora

---
+++ js/javascript.js
@@ -0,0 +1,81 @@
+let kliknietaWatosc;
+
+function kliknietoW0() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "0";
+    kliknietaWatosc = 0;
+}
+
+function kliknietoW1() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "1";
+    kliknietaWatosc = 1;
+}
+
+function kliknietoW2() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "2";
+    kliknietaWatosc = 2;
+}
+
+function kliknietoW3() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "3";
+    kliknietaWatosc = 3;
+}
+
+function kliknietoW4() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "4";
+    kliknietaWatosc = 4;
+}
+
+function kliknietoW5() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "5";
+    kliknietaWatosc = 5;
+}
+
+function kliknietoW6() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "6";
+    kliknietaWatosc = 6;
+}
+
+function kliknietoW7() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "7";
+    kliknietaWatosc = 7;
+}
+
+function kliknietoW8() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "8";
+    kliknietaWatosc = 8;
+}
+
+function kliknietoW9() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "9";
+    kliknietaWatosc = 9;
+}
+
+function dzielenie() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "/";
+}
+
+function mnozenie() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "*";
+}
+
+function dodawanie() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "+";
+}
+
+function odejmowanie() {
+    const screen = document.getElementById("screen");
+    screen.innerHTML = "-";
+}
