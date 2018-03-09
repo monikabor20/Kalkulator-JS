ad183b1b37aefd5eaabc7bb32b8fd086a3002990
Monika Borawska <monikabor20@wp.pl>
2018-03-09 12:16:54 +0100

    Dodanie funkcji do dzialan i rowna sie

--- js/javascript.js
+++ js/javascript.js
@@ -1,81 +1,41 @@
-let kliknietaWatosc;
+let kliknietaWatoscX;
+let kliknieteDzialanie;
+let kliknietaWatoscY;
 
-function kliknietoW0() {
+
+function kliknietoWLiczbe(liczba) {
     const screen = document.getElementById("screen");
-    screen.innerHTML = "0";
-    kliknietaWatosc = 0;
+    screen.innerHTML = liczba;
+    if (!kliknieteDzialanie) {
+        kliknietaWatoscX = liczba;
+    } else {
+        kliknietaWatoscY = liczba;
+    }
 }
 
-function kliknietoW1() {
+function kliknietoWDzialanie(dzialanie) {
     const screen = document.getElementById("screen");
-    screen.innerHTML = "1";
-    kliknietaWatosc = 1;
+    screen.innerHTML = dzialanie;
+    kliknieteDzialanie = dzialanie;
 }
 
-function kliknietoW2() {
+function kliknietoWRownaSie() {
     const screen = document.getElementById("screen");
-    screen.innerHTML = "2";
-    kliknietaWatosc = 2;
+
+    switch (kliknieteDzialanie) {
+        case "+":
+            screen.innerHTML = kliknietaWatoscX + "+" + kliknietaWatoscY + "=" + (kliknietaWatoscX + kliknietaWatoscY);
+            break;
+        case "-":
+            screen.innerHTML = kliknietaWatoscX + "-" + kliknietaWatoscY + "=" + (kliknietaWatoscX - kliknietaWatoscY);
+            break;
+        case "*":
+            screen.innerHTML = kliknietaWatoscX + "*" + kliknietaWatoscY + "=" + (kliknietaWatoscX * kliknietaWatoscY);
+            break;
+        case "/":
+            screen.innerHTML = kliknietaWatoscX + "/" + kliknietaWatoscY + "=" + (kliknietaWatoscX / kliknietaWatoscY);
+            break;
+    }
+    
 }
 
-function kliknietoW3() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "3";
-    kliknietaWatosc = 3;
-}
-
-function kliknietoW4() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "4";
-    kliknietaWatosc = 4;
-}
-
-function kliknietoW5() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "5";
-    kliknietaWatosc = 5;
-}
-
-function kliknietoW6() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "6";
-    kliknietaWatosc = 6;
-}
-
-function kliknietoW7() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "7";
-    kliknietaWatosc = 7;
-}
-
-function kliknietoW8() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "8";
-    kliknietaWatosc = 8;
-}
-
-function kliknietoW9() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "9";
-    kliknietaWatosc = 9;
-}
-
-function dzielenie() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "/";
-}
-
-function mnozenie() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "*";
-}
-
-function dodawanie() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "+";
-}
-
-function odejmowanie() {
-    const screen = document.getElementById("screen");
-    screen.innerHTML = "-";
-}
